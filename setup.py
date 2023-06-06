0ab7789c1860b668b4f6efdf2511d59ce37b879e
dhuck <dhuck@tinycact.us>
2023-06-06 13:59:12 -0500

    simple setup

---
+++ setup.py
@@ -0,0 +1,10 @@
+from distutils.core import setup
+
+setup(
+    name='Fabman API',
+    version='1.0',
+    description='Library for interfacing with the Fabman API',
+    author='Davin Lawrence',
+    author_email='fabman-api@tinycact.us',
+    packages=['fabman-api']
+)
