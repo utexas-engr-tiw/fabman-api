bc3eeef1bb091b6b029ec3fdf462e4bd24171fcf
dhuck <dhuck@tinycact.us>
2023-06-16 12:57:16 -0500

    adjust for key requirements

--- tests/test_sanity.py
+++ tests/test_sanity.py
@@ -9,22 +9,21 @@ import fabman
 
 conf = configparser.ConfigParser()
 conf.read('config.ini')
+key = conf['FABMAN']['API_TOKEN']
 
 class SanityTest(unittest.TestCase):
     def test_sanity(self):
         self.assertEqual(1, 1)
 
     def test_fabman_import(self):
-        fm = fabman.Fabman()
+        fm = fabman.Fabman(key)
         self.assertEqual(fm.__class__.__name__, 'Fabman')
         
-    def test_sanity_token(self):
-        key = conf['FABMAN']['API_TOKEN']
+    def test_sanity_token(self):        
         fm = fabman.Fabman(key)
         self.assertEqual(fm.get_api_token(), key)
         
     def test_user_me(self):
-        key = conf['FABMAN']['API_TOKEN']
         fm = fabman.Fabman(key)
         user = fm.get_user_me()
         self.assertEqual(user["members"][0]["memberNumber"], "1")
