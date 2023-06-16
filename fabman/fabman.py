fc189bf504b08ede1fed44cf1fae5002effe0ab0
dhuck <dhuck@tinycact.us>
2023-06-16 13:43:46 -0500

    added bookings get points

--- fabman/fabman.py
+++ fabman/fabman.py
@@ -85,9 +85,11 @@ class Fabman:
 
         Args:
             limit (int, optional): Numberof accounts to return. Defaults to 50.
-            orderBy (str, optional): Field to order by. Can be 'name', 'id', 'plan', 'createdAt', 'trialEndDate'. Defaults to 'name'.
+            orderBy (str, optional): Field to order by. Can be 'name', 'id', 'plan', 'createdAt', 'trialEndDate'. 
+            Defaults to 'name'.
             order (str, optional): Order ascending or descending. Defaults to 'asc'.
-            **kwargs: Additional arguments to filter the results. Valid arguments are 'offset', 'embed', 'q', 'eq', 'trial', 'countryCode', 'deleted'. Refer to the fabman API documentation for more information.
+            **kwargs: Additional arguments to filter the results. Valid arguments are 'offset', 'embed', 'q', 'eq', 
+            'trial', 'countryCode', 'deleted'. Refer to the fabman API documentation for more information.
 
         Raises:
             KeyError: Raised when an incorrect value has been passed to one of the keyword arguments.
@@ -145,7 +147,8 @@ class Fabman:
             limit (int, optional): Number of accounts to return. Defaults to 50.
             offset (int, optional): Offset in the list of all accounts. Defaults to 0.
             account (int, optional): Give a specific account id. Defaults to -1.
-            embed (Optional[str], optional): Embed additional information in the returned information. Only valid options are None or 'member'. Defaults to None.
+            embed (Optional[str], optional): Embed additional information in the returned information. Only valid 
+            options are None or 'member'. Defaults to None.
 
         Raises:
             KeyError: _description_
@@ -169,7 +172,8 @@ class Fabman:
 
         Args:
             id (int): ID of account in question
-            embed (Optional[str], optional): Embed additional information. Can be either 'member' or None. Defaults to None.
+            embed (Optional[str], optional): Embed additional information. Can be either 'member' or None. Defaults to 
+            None.
 
         Raises:
             NotImplementedError: Endpoint returns a 404 and is seemingly broken at time of development.
@@ -185,7 +189,8 @@ class Fabman:
 
         Args:
             id (int): ID of account in question
-            embed (Optional[str], optional): Embed additional information. Can be either 'member' or None. Defaults to None.
+            embed (Optional[str], optional): Embed additional information. Can be either 'member' or None. Defaults to
+            None.
 
         Raises:
             NotImplementedError: Endpoint returns a 404 and is seemingly broken at time of development.
@@ -195,3 +200,114 @@ class Fabman:
         """
         raise NotImplementedError(
             'Though documented, Fabman returns 404 not found on this endpoint.')
+
+    def get_bookings(self, account: Optional[str] = None, space: Optional[str] = None, resource: Optional[str] = None,
+                     member: Optional[str] = None, fromDateTime: Optional[str] = None,
+                     untilDateTime: Optional[str] = None, state: Optional[str] = None, resolve: Optional[str] = None,
+                     order='desc', limit: int = 50, offset: int = 0, summary: bool = False) -> Union[Dict, List]:
+        """Returns a list of bookings matching the query.
+
+        Args:
+            account (Optional[str], optional): Account id to inspect. Defaults to None.
+            space (Optional[str], optional): The space which will hold the bookings. Defaults to None.
+            resource (Optional[str], optional): The resource which is booked. Defaults to None.
+            member (Optional[str], optional): The member ID of the member holding the bookings. Defaults to None.
+            fromDateTime (Optional[str], optional): Beginning DateTime. Defaults to None.
+            untilDateTime (Optional[str], optional): Ending DateTime. Defaults to None.
+            state (Optional[str], optional): One of 'pending,' 'confirmed,' or 'cancelled'. Defaults to None.
+            resolve (Optional[str], optional): Return relationship details instead of just the id. Can be either 
+            'resource' or 'member'. Defaults to None.  
+            order (str, optional): Either 'asc' or 'desc'. Defaults to 'desc'.
+            limit (int, optional): Number of entries to return. Defaults to 50.
+            offset (int, optional): Offset of entries. Can be used for pagination. Defaults to 0.
+            summary (bool, optional): Add headers with summary information about found records. Defaults to False.
+
+        Returns:
+            Union[Dict, List]: _description_
+        """
+        url_path = f'/bookings?order={order}&limit={limit}&offset={offset}&summary={summary}'
+        if state and state in ['pending', 'confirmed', 'cancelled']:
+            url_path += f'&state={state}'
+        elif state:
+            raise KeyError(state)
+        if resolve and resolve in ['resource', 'member']:
+            url_path += f'&resolve={resolve}'
+        elif resolve:
+            raise KeyError(resolve)
+        if account:
+            url_path += f'&account={account}'
+        if space:
+            url_path += f'&space={space}'
+        if resource:
+            url_path += f'&resource={resource}'
+        if member:
+            url_path += f'&member={member}'
+        if fromDateTime:
+            url_path += f'&fromDateTime={fromDateTime}'
+        if untilDateTime:
+            url_path += f'&untilDateTime={untilDateTime}'
+        return self.__get(url_path)
+
+    def get_bookings_by_id(self, id: int, embed: Optional[str] = None) -> Union[Dict, List]:
+        """Get the booking information for a single booking id.
+
+        Args:
+            id (int): ID of the booking in question
+            embed (Optional[str], optional): Allows you to embed related entities to reduce the number of requests 
+            needed Can be either 'member' or 'resource'. Defaults to None.
+
+        Returns:
+            Union[Dict, List]: Dictionary containing information of the booking.
+        """
+        url_path = f'/bookings/{id}'
+        if embed and embed in ['member', 'resource']:
+            url_path += f'?embed={embed}'
+        elif embed:
+            raise KeyError(embed)
+
+        return self.__get(url_path)
+
+    def get_bookings_export(self, account: Optional[str] = None, space: Optional[str] = None, 
+                            resource: Optional[str] = None, member: Optional[str] = None, 
+                            fromDateTime: Optional[str] = None, untilDateTime: Optional[str] = None, 
+                            state: Optional[str] = None, resolve: Optional[str] = None, 
+                            order: str = 'desc') -> Union[Dict, List, str]:
+        """Returns a download link for a CSV document containing the information of _all_ bookings matching the query.
+
+        Args:
+            account (Optional[str], optional): Account id to inspect. Defaults to None.
+            space (Optional[str], optional): The space which will hold the bookings. Defaults to None.
+            resource (Optional[str], optional): The resource which is booked. Defaults to None.
+            member (Optional[str], optional): The member ID of the member holding the bookings. Defaults to None.
+            fromDateTime (Optional[str], optional): Beginning DateTime. Defaults to None.
+            untilDateTime (Optional[str], optional): Ending DateTime. Defaults to None.
+            state (Optional[str], optional): One of 'pending,' 'confirmed,' or 'cancelled'. Defaults to None.
+            resolve (Optional[str], optional): Return relationship details instead of just the id. Can be either 
+            'resource' or 'member'. Defaults to None.  
+            order (str, optional): Either 'asc' or 'desc'. Defaults to 'desc'.
+
+        Returns:
+            Union[Dict, List]: _description_
+        """
+        url_path = f'/bookings/export?order={order}'
+        if state and state in ['pending', 'confirmed', 'cancelled']:
+            url_path += f'&state={state}'
+        elif state:
+            raise KeyError(state)
+        if resolve and resolve in ['resource', 'member']:
+            url_path += f'&resolve={resolve}'
+        elif resolve:
+            raise KeyError(resolve)
+        if account:
+            url_path += f'&account={account}'
+        if space:
+            url_path += f'&space={space}'
+        if resource:
+            url_path += f'&resource={resource}'
+        if member:
+            url_path += f'&member={member}'
+        if fromDateTime:
+            url_path += f'&fromDateTime={fromDateTime}'
+        if untilDateTime:
+            url_path += f'&untilDateTime={untilDateTime}'
+        return self.__get(url_path)
