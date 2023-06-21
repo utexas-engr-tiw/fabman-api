71cbf7159cbffa98f4641f2c8e9645f5202d7e0c
dhuck <dhuck@tinycact.us>
2023-06-21 12:14:21 -0500

    added classifiers

--- setup.py
+++ setup.py
@@ -7,5 +7,19 @@ setup(
     author='Davin Lawrence',
     author_email='fabman-api@tinycact.us',
     packages=['fabman'],
-    install_requires=['requests']
+    install_requires=['requests'],
+    classifiers=[
+        "Development Status :: 2 - Pre-Alpha",
+        "Environment :: Console",
+        "Intended Audience :: Developers",
+        "Intended Audience :: Education",
+        "License :: OSI Approved :: MIT License",
+        "Natural Language :: English",
+        "Programming Language :: Python :: 3 :: Only",
+        "Programming Language :: Python :: 3.8",
+        "Programming Language :: Python :: 3.9",
+        "Programming Language :: Python :: 3.10",
+        "Programming Language :: Python :: 3.11",
+        "Topic :: Education"
+    ]
 )
