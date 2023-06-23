49ba4387095c06a4c770613923f40513ed0825d4
dhuck <dhuck@tinycact.us>
2023-06-23 11:55:34 -0500

    added get resource

--- fabman/fabman.py
+++ fabman/fabman.py
@@ -6,6 +6,7 @@ import warnings
 
 from fabman.requester import Requester
 from fabman.member import Member
+from fabman.resources import Resource
 
 
 class Fabman(object):
@@ -20,7 +21,7 @@ class Fabman(object):
 
         Args:
             access_token (str): The access token to access the API
-            base_url (str, optional): The base url of the API. Defaults to 
+            base_url (str, optional): The base url of the API. Defaults to
             "https://api.fabman.io/v1".
         """
 
@@ -47,7 +48,7 @@ class Fabman(object):
         self.__requester = Requester(base_url, access_token)
 
     def create_member(self, **kwargs) -> Member:
-        """Creates a new member in the Fabman database. 
+        """Creates a new member in the Fabman database.
         Calls "POST /members"
         Documentation: https://fabman.io/api/v1/documentation#/members/postMembers
 
@@ -57,14 +58,14 @@ class Fabman(object):
         raise NotImplementedError("create_member not implemented yet")
 
     def get_members(self, **kwargs):
-        """Get all of the members in the Fabman database. Can specify filters, 
+        """Get all of the members in the Fabman database. Can specify filters,
         search string, result limits, offsets, and sorting. Refer to the Fabman API
         documentation.
 
         calls "GET /members"
         documentation https://fabman.io/api/v1/documentation#/members/getMembers
         """
-        raise NotImplementedError("get_members not implemented yet")
+        raise NotImplementedError("get_members not implemented yet. Awaiting Pagination")
 
     def get_member(self, member_id: int, **kwargs):
         """Retrieves a member from the API give their id
@@ -82,11 +83,35 @@ class Fabman(object):
 
         return Member(self.__requester, response.json())
 
+    def get_resources(self, **kwargs):
+        """
+        Get list of available resources (e.g. doors, printers, etc.) Limit, offset,
+        and a number of filters are available. Refer to the appropriate documentation.
+        calls "GET /resources"
+        Documentation: https://fabman.io/api/v1/documentation#/resources/getResources
+        """
+        raise NotImplementedError("get_resources not implemented yet. Awaiting Pagination")
+
+    def get_resource(self, resource_id: int, **kwargs):
+        """
+        Get a single resource by its ID. Embed information is also available.
+        
+        Calls "GET /resources/{id}"
+        Documentation: https://fabman.io/api/v1/documentation#/resources/getResourcesId
+        """
+        uri = f"/resources/{resource_id}"
+
+        response = self.__requester.request(
+            "GET", uri, _kwargs=kwargs
+        )
+
+        return Resource(self.__requester, response.json())
+
     def get_user(self, **kwargs):
         """Gets authenticated user information from the API as Member object. Does
         not returns state and superuser information.
         calls "GET /user/me"
-        Documentation: https://fabman.io/api/v1/documentation#/user/getUserMe
+        Documentation: https: // fabman.io/api/v1/documentation  # /user/getUserMe
         """
         uri = "/user/me"
 
--- tests/test_fabman.py
+++ tests/test_fabman.py
@@ -1,5 +1,5 @@
 """Tests for the fabman module."""
-# pylint: disable=missing-function-docstring, missing-class-docstring, invalid-name
+# pylint: disable=missing-function-docstring, missing-class-docstring, invalid-name, unused-argument
 import unittest
 import warnings
 
@@ -7,31 +7,32 @@ import requests_mock
 
 from fabman import Fabman
 from fabman.member import Member
+from fabman.resources import Resource
 from tests import settings
 from tests.util import register_uris
 
 
 @requests_mock.Mocker()
-class TestFabman(unittest.TestCase):  # pylint: disable=missing-class-docstring
+class TestFabman(unittest.TestCase):
     def setUp(self):
         self.fabman = Fabman(settings.API_KEY)
 
     # Test initializing the Fabman instance
-    def test_init_no_api_key(self, m):  # pylint: disable=missing-function-docstring, unused-argument
+    def test_init_no_api_key(self, m):
         with self.assertRaises(
             ValueError,
             msg="No access token provided"
         ):
             Fabman(None)
 
-    def test_init_empty_api_key(self, m):  # pylint: disable=missing-function-docstring, unused-argument
+    def test_init_empty_api_key(self, m):
         with self.assertRaises(
             ValueError,
             msg="No access token provided"
         ):
             Fabman("")
 
-    def test_init_warnings_for_http(self, m):  # pylint: disable=missing-function-docstring, unused-argument
+    def test_init_warnings_for_http(self, m):
         with warnings.catch_warnings(record=True):
             Fabman(settings.API_KEY, settings.BASE_URL_AS_HTTP)
             self.assertRaises(
@@ -42,7 +43,7 @@ class TestFabman(unittest.TestCase):  # pylint: disable=missing-class-docstring
                 )
             )
 
-    def test_init_warnings_for_bad_url(self, m):  # pylint: disable=missing-function-docstring, unused-argument
+    def test_init_warnings_for_bad_url(self, m):
         with warnings.catch_warnings(record=True):
             Fabman(settings.API_KEY, settings.BASE_URL_AS_INVALID)
             self.assertRaises(
@@ -52,7 +53,7 @@ class TestFabman(unittest.TestCase):  # pylint: disable=missing-class-docstring
                 )
             )
 
-    def test_get_member(self, m):  # pylint: disable=missing-function-docstring
+    def test_get_member(self, m):
         register_uris({"fabman": ["get_by_id"]}, m)
 
         member = self.fabman.get_member(1)
@@ -60,7 +61,12 @@ class TestFabman(unittest.TestCase):  # pylint: disable=missing-class-docstring
         self.assertTrue(hasattr(member, "id"))
         self.assertTrue(hasattr(member, "firstName"))
 
-    def test_get_member_with_embed_str(self, m):  # pylint: disable=missing-function-docstring
+    def test_get_member_with_embed_str(self, m):
+        """
+        Tests the ability to embed a single resource as a string. Primarily tests 
+        the functionality of the Requester child object. Embedding does not need 
+        to be checked for other resources, as they all use the same Requester.
+        """
         register_uris({"fabman": ["get_by_id_with_embed_str"]}, m)
 
         member = self.fabman.get_member(1, embed="memberPackages")
@@ -68,7 +74,12 @@ class TestFabman(unittest.TestCase):  # pylint: disable=missing-class-docstring
         self.assertTrue(hasattr(member, "id"))
         self.assertTrue(hasattr(member, "_embedded"))
 
-    def test_get_member_with_embed_single_list(self, m):  # pylint: disable=missing-function-docstring
+    def test_get_member_with_embed_single_list(self, m):
+        """
+        Tests the ability to embed a single resource in a list. Primarily tests 
+        the functionality of the Requester child object. Embedding does not need 
+        to be checked for other resources, as they all use the same Requester.
+        """
         register_uris({"fabman": ["get_by_id_with_embed_str"]}, m)
 
         member = self.fabman.get_member(1, embed=["memberPackages"])
@@ -80,6 +91,11 @@ class TestFabman(unittest.TestCase):  # pylint: disable=missing-class-docstring
             "memberPackages" in member._embedded)  # pylint: disable=protected-access
 
     def test_get_member_with_embed_multi_list(self, m):
+        """
+        Tests the ability to embed multiple resource in a list. Primarily tests 
+        the functionality of the Requester child object. Embedding does not need 
+        to be checked for other resources, as they all use the same Requester.
+        """
         register_uris({"fabman": ["get_by_id_with_embed_list_multi"]}, m)
 
         member = self.fabman.get_member(
@@ -92,7 +108,7 @@ class TestFabman(unittest.TestCase):  # pylint: disable=missing-class-docstring
         self.assertTrue("memberPackages" in embeds)
         self.assertTrue("trainings" in embeds)
 
-    def test_get_user_me(self, m):  # pylint: disable=missing-function-docstring
+    def test_get_user_me(self, m):
         register_uris({"fabman": ["get_user_me"]}, m)
 
         member = self.fabman.get_user()
@@ -100,6 +116,17 @@ class TestFabman(unittest.TestCase):  # pylint: disable=missing-class-docstring
         self.assertTrue(hasattr(member, "id"))
         self.assertTrue(hasattr(member, "firstName"))
         self.assertTrue(hasattr(member, "account"))
+
+    def test_get_resource(self, m):
+        register_uris({"fabman": ["get_resource_id"]}, m)
+
+        resource = self.fabman.get_resource(1)
+        self.assertIsInstance(resource, Resource)
+        self.assertTrue(hasattr(resource, "id"))
+        self.assertTrue(hasattr(resource, "name"))
+
+    def test_get_resources_embed(self, m):
+        register_uris({"fabman": ["get_resources_embed"]}, m)
 
 
 if __name__ == "__main__":
