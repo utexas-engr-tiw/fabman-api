654317c86c1cf5723ce6aee8bf0db6dbee72c7b8
dhuck <dhuck@tinycact.us>
2023-06-22 23:40:42 -0500

    added CaseInsensitiveDict to clean_headers types

--- fabman/util.py
+++ fabman/util.py
@@ -1,6 +1,7 @@
 """General Utility Functions to be used throughout the package"""
 
-from typing import Any
+from typing import Any, Union
+from requests.structures import CaseInsensitiveDict
 
 
 def is_multivalued(value: Any):
@@ -21,7 +22,7 @@ def is_multivalued(value: Any):
         return False
 
 
-def clean_headers(headers: dict):
+def clean_headers(headers: Union[dict, CaseInsensitiveDict]):
     """Cleans the headers to hide sensitive information in logs. 
     Originally defined in canvasapi/util.py: 
     https://github.com/ucfopen/canvasapi/blob/develop/canvasapi/util.py#L228
