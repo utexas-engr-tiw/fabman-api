dc74e051de2abddeaf61771e8a72adcc35812c9a
dhuck <dhuck@tinycact.us>
2023-06-23 14:59:09 -0500

    change get methods to return json() add get_device_change

--- fabman/member.py
+++ fabman/member.py
@@ -2,7 +2,6 @@
 # pylint: disable=too-many-public-methods, line-too-long
 import requests
 from fabman.fabman_object import FabmanObject
-from fabman.util import combine_kwargs
 
 
 class Member(FabmanObject):
@@ -119,11 +118,12 @@ class Member(FabmanObject):
         calls "GET /members/{id}/balance-items"
         Documentation: https://fabman.io/api/v1/documentation#/members/getMembersIdBalanceitems
         """
-        return self._requester.request(
+        response = self._requester.request(
             "GET", f"/members/{self.id}/balance-items",
             _kwargs=kwargs,
-        ).json()
-        
+        )
+
+        return response.json()
 
     def get_changes(self, **kwargs) -> requests.Response:
         """
@@ -131,10 +131,12 @@ class Member(FabmanObject):
         calls "GET /members/{id}/changes"
         Documentation: https://fabman.io/api/v1/documentation#/members/getMembersIdChanges
         """
-        return self._requester.request(
+        response = self._requester.request(
             "GET", f"/members/{self.id}/changes",
             _kwargs=kwargs,
         )
+
+        return response.json()
 
     def get_credits(self, **kwargs) -> requests.Response:
         """
@@ -142,10 +144,12 @@ class Member(FabmanObject):
         calls "GET /members/{id}/credits"
         Documentation: https://fabman.io/api/v1/documentation#/members/getMembersIdCredits
         """
-        return self._requester.request(
+        response = self._requester.request(
             "GET", f"/members/{self.id}/credits",
             _kwargs=kwargs,
         )
+
+        return response.json()
 
     def get_credit_by_id(self, credit_id: int, **kwargs) -> requests.Response:
         """
@@ -153,10 +157,12 @@ class Member(FabmanObject):
         calls "GET /members/{id}/credits/{creditId}"
         Documentation: https://fabman.io/api/v1/documentation#/members/getMembersIdCreditsCreditId
         """
-        return self._requester.request(
+        response = self._requester.request(
             "GET", f"/members/{self.id}/credits/{credit_id}",
             _kwargs=kwargs,
         )
+
+        return response.json()
 
     def get_credit_uses_by_id(self, credit_id: int, **kwargs) -> requests.Response:
         """
@@ -164,32 +170,51 @@ class Member(FabmanObject):
         calls "GET /members/{id}/credits/{creditId}/uses"
         Documentation: https://fabman.io/api/v1/documentation#/members/getMembersIdCreditsCreditIdUses
         """
-        return self._requester.request(
+        response = self._requester.request(
             "GET", f"/members/{self.id}/credits/{credit_id}/uses",
             _kwargs=kwargs,
         )
 
+        return response.json()
+
     def get_device(self, **kwargs) -> requests.Response:
         """
-        Retrieves the devices of a member
+        Retrieves the device of a member used to authenticate on a bridge.
         calls "GET /members/{id}/devices"
         Documentation: https://fabman.io/api/v1/documentation#/members/getMembersIdDevice
         """
-        return self._requester.request(
+        response = self._requester.request(
             "GET", f"/members/{self.id}/device",
             _kwargs=kwargs,
         )
 
+        return response.json()
+
     def get_device_changes(self, **kwargs) -> requests.Response:
         """
         Retrieves the device changes of a member
-        calls "GET /members/{id}/device-changes"
+        calls "GET /members/{id}/device/changes"
         Documentation: https://fabman.io/api/v1/documentation#/members/getMembersIdDeviceChanges
         """
-        return self._requester.request(
+        response = self._requester.request(
             "GET", f"/members/{self.id}/device/changes",
             _kwargs=kwargs,
         )
+
+        return response.json()
+
+    def get_device_change(self, change_id: int, **kwargs) -> requests.Response:
+        """
+        Retrieves a device change of a member given the change ID
+        calls "GET /members/{id}/device/changes/{changeId}"
+        Documentation: https://fabman.io/api/v1/documentation#/members/getMembersIdDeviceChangesChangeId
+        """
+        response = self._requester.request(
+            "GET", f"/members/{self.id}/device/changes/{change_id}",
+            _kwargs=kwargs,
+        )
+
+        return response.json()
 
     def get_export(self, **kwargs) -> requests.Response:
         """
@@ -205,21 +230,25 @@ class Member(FabmanObject):
         calls "GET /members/{id}/invitations"
         Documentation: https://fabman.io/api/v1/documentation#/members/getMembersIdInvitation
         """
-        return self._requester.request(
+        response = self._requester.request(
             "GET", f"/members/{self.id}/invitation",
             _kwargs=kwargs,
         )
 
+        return response.json()
+
     def get_key(self, **kwargs):
         """
-        Retrieves the key of a member
+        Retrieves the keycard number of a member
         calls "GET /members/{id}/key"
         Documentation: https://fabman.io/api/v1/documentation#/members/getMembersIdKey
         """
-        return self._requester.request(
+        response = self._requester.request(
             "GET", f"/members/{self.id}/key",
             _kwargs=kwargs,
         )
+
+        return response.json()
 
     def get_packages(self, **kwargs):
         """
@@ -227,10 +256,12 @@ class Member(FabmanObject):
         calls "GET /members/{id}/packages"
         Documentation: https://fabman.io/api/v1/documentation#/members/getMembersIdPackages
         """
-        return self._requester.request(
+        response = self._requester.request(
             "GET", f"/members/{self.id}/packages",
             _kwargs=kwargs,
         )
+
+        return response.json()
 
     def get_package(self, member_package_id: int, **kwargs) -> requests.Response:
         """
@@ -238,10 +269,12 @@ class Member(FabmanObject):
         calls "GET /members/{id}/packages/{memberPackageId}"
         Documentation: https://fabman.io/api/v1/documentation#/members/getMembersIdPackagesMemberPackageId
         """
-        return self._requester.request(
+        response = self._requester.request(
             "GET", f"/members/{self.id}/packages/{member_package_id}",
             _kwargs=kwargs,
         )
+
+        return response.json()
 
     def get_payment_account(self, **kwargs) -> requests.Response:
         """
@@ -249,10 +282,12 @@ class Member(FabmanObject):
         calls "GET /members/{id}/payment-account"
         Documentation: https://fabman.io/api/v1/documentation#/members/getMembersIdPaymentAccount
         """
-        return self._requester.request(
+        response = self._requester.request(
             "GET", f"/members/{self.id}/payment-account",
             _kwargs=kwargs,
         )
+
+        return response.json()
 
     def get_payment_method(self, **kwargs) -> requests.Response:
         """
@@ -260,10 +295,12 @@ class Member(FabmanObject):
         calls "GET /members/{id}/payment-method"
         Documentation: https://fabman.io/api/v1/documentation#/members/getMembersIdPaymentMethod
         """
-        return self._requester.request(
+        response = self._requester.request(
             "GET", f"/members/{self.id}/payment-method",
             _kwargs=kwargs,
         )
+
+        return response.json()
 
     def get_payment_method_mandate_preview(self, **kwargs) -> requests.Response:
         """
@@ -271,10 +308,12 @@ class Member(FabmanObject):
         calls "GET /members/{id}/payment-method-mandate-preview"
         Documentation: https://fabman.io/api/v1/documentation#/members/getMembersIdPaymentMethodMandatePreview
         """
-        return self._requester.request(
+        response = self._requester.request(
             "GET", f"/members/{self.id}/payment-method-mandate-preview",
             _kwargs=kwargs,
         )
+
+        return response.json()
 
     def get_privileges(self, **kwargs) -> requests.Response:
         """
@@ -282,10 +321,12 @@ class Member(FabmanObject):
         calls "GET /members/{id}/privileges"
         Documentation: https://fabman.io/api/v1/documentation#/members/getMembersIdPrivileges
         """
-        return self._requester.request(
+        response = self._requester.request(
             "GET", f"/members/{self.id}/privileges",
             _kwargs=kwargs,
         )
+
+        return response.json()
 
     def get_trained_resources(self, **kwargs) -> requests.Response:
         """
@@ -293,10 +334,12 @@ class Member(FabmanObject):
         calls "GET /members/{id}/trained-resources"
         Documentation: https://fabman.io/api/v1/documentation#/members/getMembersIdTrainedResources
         """
-        return self._requester.request(
+        response = self._requester.request(
             "GET", f"/members/{self.id}/trained-resources",
             _kwargs=kwargs,
         )
+
+        return response.json()
 
     def get_trainings(self, **kwargs) -> requests.Response:
         """
@@ -304,10 +347,12 @@ class Member(FabmanObject):
         calls "GET /members/{id}/trainings"
         Documentation: https://fabman.io/api/v1/documentation#/members/getMembersIdTrainings
         """
-        return self._requester.request(
+        response = self._requester.request(
             "GET", f"/members/{self.id}/trainings",
             _kwargs=kwargs,
         )
+
+        return response.json()
 
     def get_training(self, training_id: int, **kwargs) -> requests.Response:
         """
@@ -315,7 +360,9 @@ class Member(FabmanObject):
         calls "GET /members/{id}/trainings/{trainingId}"
         Documentation: https://fabman.io/api/v1/documentation#/members/getMembersIdTrainingsTrainingId
         """
-        return self._requester.request(
+        response = self._requester.request(
             "GET", f"/members/{self.id}/trainings/{training_id}",
             _kwargs=kwargs,
         )
+
+        return response.json()
