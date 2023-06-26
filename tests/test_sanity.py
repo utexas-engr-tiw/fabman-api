997d8cdf2d320a97abb762f4082f79934871a412
dhuck <dhuck@tinycact.us>
2023-06-26 15:05:18 -0500

    fix sanity check to work in cloud

--- tests/test_sanity.py
+++ tests/test_sanity.py
@@ -1,24 +1,22 @@
-import os
-import sys
-import configparser
+"""Sanity Checks"""
 import unittest
 
 import fabman
 
-conf = configparser.ConfigParser()
-conf.read('config.ini')
-key = conf['FABMAN']['API_TOKEN']
 
 class SanityTest(unittest.TestCase):
+    """Quick sanity check to make sure tests are working fine"""
+
     def test_sanity(self):
         """Quick sanity check to make sure tests are working fine"""
         self.assertEqual(1, 1)
 
     def test_fabman_import(self):
         """Quick sanity to check to ensure fabman is imported correctly"""
-        fm = fabman.Fabman(key)
-        self.assertEqual(fm.__class__.__name__, 'Fabman')
-        
-        
+        key = 123
+        fab = fabman.Fabman(key)
+        self.assertEqual(fab.__class__.__name__, 'Fabman')
+
+
 if __name__ == '__main__':
     unittest.main()
