acf506ff96c2d31c8f593a327debaf11d5a6f2e4
dhuck <dhuck@tinycact.us>
2023-06-26 15:57:43 -0500

    include python 3.7

--- setup.py
+++ setup.py
@@ -16,6 +16,7 @@ setup(
         "License :: OSI Approved :: MIT License",
         "Natural Language :: English",
         "Programming Language :: Python :: 3 :: Only",
+        "Programming Language :: Python :: 3.7",
         "Programming Language :: Python :: 3.8",
         "Programming Language :: Python :: 3.9",
         "Programming Language :: Python :: 3.10",
