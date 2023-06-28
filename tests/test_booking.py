f765588a63de797795c03292190009272b0c7259
dhuck <dhuck@tinycact.us>
2023-06-28 12:42:16 -0500

    test bookings

--- tests/test_booking.py
+++ tests/test_booking.py
@@ -8,7 +8,7 @@ import requests_mock
 from fabman import Fabman
 from fabman.booking import Booking
 from tests import settings
-from tests.util import register_uris
+from tests.util import register_uris, validate_update
 
 
 @requests_mock.Mocker()
@@ -23,3 +23,21 @@ class TestBooking(unittest.TestCase):
 
     def test_sanity(self, m):
         self.assertEqual(1, 1)
+
+    def test_delete(self, m):
+        register_uris({"booking": ["delete"]}, m)
+
+        resp = self.booking.delete()
+        self.assertTrue(m.called)
+        self.assertTrue(resp.status_code == 204)
+
+    def test_update(self, m):
+        m.register_uri(
+            "PUT",
+            f"{settings.BASE_URL_WITH_VERSION}/bookings/1",
+            text=validate_update,
+            status_code=200,
+        )
+
+        self.booking.update()
+        self.assertTrue(m.called)
