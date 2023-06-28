a3615cb8f956398bce322630303a5638e065884f
dhuck <dhuck@tinycact.us>
2023-06-27 22:58:12 -0500

    training course tests

--- tests/test_training_course.py
+++ tests/test_training_course.py
@@ -8,7 +8,7 @@ import requests_mock
 from fabman import Fabman
 from fabman.training_course import TrainingCourse
 from tests import settings
-from tests.util import register_uris
+from tests.util import register_uris, validate_update
 
 
 @requests_mock.Mocker()
@@ -23,3 +23,26 @@ class TestTrainingCourse(unittest.TestCase):
 
     def test_sanity(self, m):
         self.assertEqual(1, 1)
+
+    def test_instance(self, m):
+        self.assertIsInstance(self.training_course, TrainingCourse)
+        string = str(self.training_course)
+        self.assertTrue(string == "TrainingCourse #1: Bajoran Worker Training")
+
+    def test_delete(self, m):
+        register_uris({"training_course": ["delete"]}, m)
+
+        response = self.training_course.delete()
+
+        self.assertTrue(response)
+
+    def test_update(self, m):
+        m.register_uri(
+            "PUT",
+            f"{settings.BASE_URL_WITH_VERSION}/training-courses/1",
+            text=validate_update,
+            status_code=200,
+        )
+
+        self.training_course.update(title="Bajoran Worker Training")
+        self.assertTrue(m.called)
