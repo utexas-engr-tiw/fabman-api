9f8cc07ad9e288550de5598cfd8bf2c58576ca23
dhuck <dhuck@tinycact.us>
2023-06-29 10:51:08 -0500

    test paginated_list

--- tests/test_paginated_list.py
+++ tests/test_paginated_list.py
@@ -6,6 +6,7 @@ import unittest
 import requests_mock
 
 from fabman import Fabman
+from fabman.member import Member
 from fabman.paginated_list import PaginatedList
 from tests import settings
 from tests.util import register_uris
@@ -33,3 +34,59 @@ class TestPaginatedList(unittest.TestCase):
         }
         link = self.paginated_list._PaginatedList__format_link(headers)
         self.assertEqual(link, "/members?limit=1&offset=1")
+
+    def test_iteration(self, m):
+        register_uris(
+            {
+                "paginated_list": ["get_members_first", "get_members_second"],
+            },
+            m,
+        )
+
+        members = self.fabman.get_members(limit=5)
+        self.assertIsInstance(members, PaginatedList)
+        items = [m for m in members]
+        self.assertEqual(len(items), 10)
+        for i, item in enumerate(items):
+            self.assertIsInstance(item, Member)
+            self.assertEqual(item.id, i + 1)
+
+    def test_negative_index(self, m):
+        register_uris({"paginated_list": ["get_members_first"]}, m)
+
+        members = self.fabman.get_members(limit=5)
+        self.assertIsInstance(members, PaginatedList)
+
+        with self.assertRaises(IndexError):
+            member = members[-1]
+
+    def test_index(self, m):
+        register_uris({"paginated_list": ["get_members_first"]}, m)
+
+        members = self.fabman.get_members(limit=5)
+        self.assertIsInstance(members, PaginatedList)
+        member = members[2]
+        self.assertIsInstance(member, Member)
+        self.assertTrue(member.id == 3)
+
+    def test_grow_index(self, m):
+        register_uris(
+            {"paginated_list": ["get_members_first", "get_members_second"]}, m
+        )
+
+        members = self.fabman.get_members(limit=5)
+        self.assertIsInstance(members, PaginatedList)
+        member = members[7]
+        self.assertIsInstance(member, Member)
+        self.assertTrue(member.id == 8)
+
+    def test_index_out_of_range(self, m):
+        register_uris(
+            {"paginated_list": ["get_members_first", "get_members_second"]}, m
+        )
+
+        members = self.fabman.get_members(limit=5)
+        self.assertIsInstance(members, PaginatedList)
+
+        with self.assertRaises(IndexError):
+            member = members[11]
