68288865ca30d142143a54fdb1dea614c9a18387
dhuck <dhuck@tinycact.us>
2023-06-30 18:21:06 -0500

    create static fabman object

--- fabman/fabman_object.py
+++ fabman/fabman_object.py
@@ -5,6 +5,50 @@ from typing import Any
 import fabman.requester
 
 
+class FabmanStaticObject(object):
+    """
+    Base class for all objects returned from the API which do not have any
+    associated methods to call. For example, Payment Info objects returned from
+    an Account are immutable
+    """
+
+    def __getattribute__(self, __name: str) -> Any:
+        return super(FabmanStaticObject, self).__getattribute__(__name)
+
+    def __init__(self, attributes: dict) -> None:
+        """Initializes the object and calls :code:`set_attributes` to dynamically
+        populate the attributes of the class
+
+        :param attributes: dictionary of attributes to set
+        :type attributes: dict
+        :return: Nothing
+        :rtype: None
+        """
+
+        self.set_attributes(attributes)
+
+    def __repr__(self) -> str:
+        classname = self.__class__.__name__
+        attrs = ", ".join(
+            [
+                f"{k}={v}"
+                for k, v in self.__dict__.items()
+                if not k.startswith("_") and k != "attributes"
+            ]
+        )
+
+        return f"<{classname} {attrs}>"
+
+    def set_attributes(self, attributes: dict):
+        """Helper method to set attributes on the object
+
+        :param attributes: dictionary of attributes
+        :type attributes: dict
+        """
+        for attr, val in attributes.items():
+            setattr(self, attr, val)
+
+
 class FabmanObject(object):
     """
     Base class for all classes representing objects returned by the API
