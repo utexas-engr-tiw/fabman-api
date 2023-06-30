3770984a788a3481c019bbb8e9ba1b2c04e140c1
dhuck <dhuck@tinycact.us>
2023-06-30 15:04:09 -0500

    exceptions documentation stub

--- docs/exceptions.rst
+++ docs/exceptions.rst
@@ -1,2 +1,2 @@
-Exceptions 
+Exceptions
 ==========
--- fabman/exceptions.py
+++ fabman/exceptions.py
@@ -20,34 +20,51 @@ class FabmanException(Exception):
 
 
 class BadRequest(FabmanException):
-    """Fabman was unable to understand the request. More information may be needed"""
+    """
+    Fabman was unable to understand the request. More information may be needed or
+    arguments may need to be fixed.
+    """
 
 
 class InvalidAccessToken(FabmanException):
-    """The access token provided was invalid"""
+    """
+    The access token provided was invalid.
+    """
 
 
 class Unauthorized(FabmanException):
-    """The access token provided was not authorized to access the resource"""
+    """
+    The access token provided was not authorized to access the resource.
+    """
 
 
 class ResourceDoesNotExist(FabmanException):
-    """The resource requested does not exist"""
+    """
+    The resource requested does not exist.
+    """
 
 
 class ForbiddenError(FabmanException):
-    """The access token provided does not have permission to access the resource"""
+    """
+    The access token provided does not have permission to access the resource.
+    """
 
 
 class Conflict(FabmanException):
-    """The request could not be completed due to a conflict with the current
-    state of the resource"""
+    """
+    The request could not be completed due to a conflict with the current
+    state of the resource.
+    """
 
 
 class UnprocessableEntity(FabmanException):
-    """The request was well-formed but was unable to be followed due to semantic errors"""
+    """
+    The request was well-formed but was unable to be followed due to semantic errors.
+    """
 
 
 class RateLimitExceeded(FabmanException):
-    """The request was valid, but too may requests have been issued from this access token.
-    Please try again later"""
+    """
+    The request was valid, but too may requests have been issued from this access token.
+    Please try again later.
+    """
